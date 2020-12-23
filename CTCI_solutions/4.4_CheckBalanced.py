39e08810efa09ed7946729f007d944d61b83df6b
Monata <mahmutasimonat@gmail.com>
2020-12-23 17:22:14 +0300

    Solution to 4.4 updated

--- CTCI_solutions/4.4_CheckBalanced.py
+++ CTCI_solutions/4.4_CheckBalanced.py
@@ -1,9 +1,6 @@
 # Implement a function to check if a binary tree is balanced. For the purposes of this question, a balanced tree is
 # defined to be a tree such that the heights of the two subtrees of any node never differ by more than one.
 
-#create minimal height binary tree from given array
-from bisect import bisect
-from collections import Counter
 
 
 class Node:
